e1ddac1663764a0846eff5a3f08e558f84edb808
nruest <ruestn@gmail.com>
2019-04-17 08:38:19 -0400

    Update to Warclight 0.8.2.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,8 @@
 //= require activestorage
 //= require turbolinks
 //
+
+//= require tether
 // Required by Blacklight
 //= require jquery
 //= require popper
@@ -27,3 +29,9 @@
 
 
 //= require_tree .
+
+
+// For blacklight_range_limit built-in JS, if you don't want it you don't need
+// this:
+//= require 'blacklight_range_limit'
+
